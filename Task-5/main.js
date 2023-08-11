1017950199e8c67a622950f744f34705be616660
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-08-11 23:32:19 +0500

    task-5 started

---
+++ Task-5/main.js
@@ -0,0 +1,74 @@
+//Funksiyani elon qilish
+//Funksiyani elon qilishda 
+// 1-(function) kalit sozi
+// 2-Funksiya nomi
+// 3-qavs ichida (parametr1, parametr2, ... parametrN)lar berish
+// 4-Funksiya scop ichiga kerakli kodni yozamiz
+// 5-Funksiyani chaqirish:=>> 
+//     Funksiya nomi (argument1, argument2,...argumentN)lar orqali chaqiriladi
+
+// Masalan:
+//    function meningFunksiyam(son1,son2) {
+//         if (son1<son2) {
+//             console.log(`${son2}  ${son1}dan katta`);
+//         } else {
+//             console.log(`${son1}  ${son2}dan katta`);
+//         }
+
+      
+//     }
+
+//     meningFunksiyam(Math.round(Math.random()*100),Math.round(Math.random()*100))
+
+
+
+
+//Uyga vazifa
+//Vazifa-1
+
+
+// function function1 (sm) {
+//     console.log(`${sm/100} m`);
+//     return sm
+// }
+// function1(Math.round(Math.random()*9999))
+
+
+//Vazifa-2
+// function function2(kg){
+//     let calc = kg / 1000
+//     console.log(`${calc} tonna`);
+//     return calc
+// }
+// function2(Math.round(Math.random()*9999))
+
+
+
+//Vazifa-3
+
+// function function3(kb){
+//     console.log(kb/1024,"Kb");
+//     return kb
+// }
+// function3(Math.round(Math.random()*9999))
+
+
+
+//Vazifa-4 
+    // function function4(kesmaA,kesmaB){
+    //     console.log(kesmaA/kesmaB);
+    // }
+    // function4 (99,33)
+
+    //5ni shartiga yaxshi tushunmadim
+    
+// //Vazifa-6
+    // function function6 (son){
+    //     let birlik = son % 10 
+    //     let onlik = (son - birlik) / 10
+    //     let natija = birlik*10
+    //     console.log(son,natija+onlik);
+    // }
+    // function6 (Math.round(Math.random()*99))
+
+    
