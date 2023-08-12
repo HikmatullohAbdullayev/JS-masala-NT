8408cb33ab2baa5392e3ab782790d4ae39e0282b
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-08-12 06:29:47 +0500

    commit

--- Task-5/main.js
+++ Task-5/main.js
@@ -1,61 +1,54 @@
 
+// Uyga vazifa
+// Vazifa - 1
 
-
-
-
-//Uyga vazifa
-//Vazifa-1
-
-
-// function function1 (sm) {
-//     console.log(`${sm/100} m`);
+// function function1(sm) {
+//     console.log(`${sm / 100} m`);
 //     return sm
 // }
-// function1(Math.round(Math.random()*9999))
+// function1(Math.round(Math.random() * 9999))
 
 
-//Vazifa-2
-// function function2(kg){
+// Vazifa - 2
+
+// function function2(kg) {
 //     let calc = kg / 1000
 //     console.log(`${calc} tonna`);
 //     return calc
 // }
-// function2(Math.round(Math.random()*9999))
+// function2(Math.round(Math.random() * 9999))
 
 
 
-//Vazifa-3
+// Vazifa - 3
 
-// function function3(kb){
-//     console.log(kb/1024,"Kb");
+
+// function function3(kb) {
+//     console.log(kb / 1024, "Kb");
 //     return kb
 // }
-// function3(Math.round(Math.random()*9999))
+// function3(Math.round(Math.random() * 9999))
 
+// Vazifa - 4
 
+// function function4(kesmaA, kesmaB) {
+//     console.log(kesmaA / kesmaB);
+// }
+// function4(99, 33)
 
-//Vazifa-4 
-    // function function4(kesmaA,kesmaB){
-    //     console.log(kesmaA/kesmaB);
-    // }
-    // function4 (99,33)
+// 5ni shartiga yaxshi tushunmadim
 
-    //5ni shartiga yaxshi tushunmadim
-    
 // //Vazifa-6
-    // function function6 (son){
-    //     let birlik = son % 10 
-    //     let onlik = (son - birlik) / 10
-    //     let natija = birlik*10
-    //     console.log(son,natija+onlik);
-    // }
-    // function6 (Math.round(Math.random()*99))
 
+// function function6(son) {
+//     let birlik = son % 10
+//     let onlik = (son - birlik) / 10
+//     let natija = birlik * 10
+//     console.log(son, natija + onlik);
+// }
+// function6(Math.round(Math.random() * 99))
 
-
-
-
-//Vazifa-7
+// Vazifa - 7
 
 // let son = (son) => {
 //     let birlik = son % 10
@@ -63,50 +56,52 @@
 //     let natija = birlik + onlik
 //     return natija
 // }
-//   console.log(son(23));
+// console.log(son(23));
 
-// //Vazifa-8 
-// let son  = (num) =>{
+// //Vazifa-8
+
+// let son = (num) => {
 //     let birlik = num % 10
-//     let onlik = (num-birlik)/10
-//     let birlik2 =birlik*10
-//     let natija = birlik2+ onlik
+//     let onlik = (num - birlik) / 10
+//     let birlik2 = birlik * 10
+//     let natija = birlik2 + onlik
 //     console.log(num);
 //     return natija
 // }
-// console.log(son(Math.round(Math.random()*99)));
+// console.log(son(Math.round(Math.random() * 99)));
 
+// Vazifa - 9
 
-//Vazifa-9
 // let son = (num) => {
 //     let onlik = num % 100
-//     let yuzlik = num-onlik
+//     let yuzlik = num - onlik
 //     return yuzlik
 // }
 // console.log(son(723));
 
-//Vazifa-10
-// let son =( num)=>{
+// Vazifa - 10
+
+// let son = (num) => {
 //     let qoldiq = num % 100
 //     let birlik = qoldiq % 10
-//     let onlik = qoldiq-birlik
+//     let onlik = qoldiq - birlik
 //     let natija = `birliklar ${birlik}   onliklar ${onlik}`
 //     console.log(`berilgan son ${num}`);
 //     console.log(natija);
 // }
-// son(Math.round(Math.random()*999))
-
+// son(Math.round(Math.random() * 999))
 
 // //Vazifa-11 
-// let son = (num) =>{
-//     let qoldiq =num % 100
-//     let yuzlik = (num-qoldiq)/100
-//     let birlik = qoldiq%10
-//     let onlik = (qoldiq- birlik)/10
+
+// let son = (num) => {
+//     let qoldiq = num % 100
+//     let yuzlik = (num - qoldiq) / 100
+//     let birlik = qoldiq % 10
+//     let onlik = (qoldiq - birlik) / 10
 //     natija = yuzlik + onlik + birlik
 //     console.log(num);
 //     console.log(`${yuzlik} + ${onlik} + ${birlik} = ${natija}`);
 //     return natija
 // }
-// console.log(son(Math.round(Math.random()*999)))
+// console.log(son(Math.round(Math.random() * 999)))
 
