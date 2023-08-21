dc425a2943ab6bf31141132185a96c1ae5a6c8f2
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-08-21 23:57:06 +0500

    Task-8 done

---
+++ Task-8/main.js
@@ -0,0 +1,74 @@
+//Vazifa-1
+// let user = (prompt) => {
+//     let str = prompt("ismingiz")
+//     let str2 = prompt("yoshingiz")
+//     let str3 = prompt("tel:")
+//     let str4 = prompt("ID raqamongiz")
+//     let natija = [
+//         str,
+//         str2,
+//         str3,
+//         str4,
+//     ] 
+//     return natija
+// }
+// console.log(user(prompt));
+
+
+//Vazifa-2
+
+
+
+//Vazifa-3
+
+// let array = (arr1,arr2,arr3) =>{
+//      arr1=["salom",23,"rreew","qwert",45]
+//  arr2=[]
+//  arr3=[]
+
+// for (let i =0;i<arr1.length;i++){
+//     if (typeof arr1[i]=== "string") {
+//         arr2[i] = arr1[i]
+//     }else if (typeof arr1[i]==="number")
+//     {
+//         arr3[i] = arr1[i] 
+//     }
+// }
+
+// console.log(arr2);
+
+// return arr3
+// }
+// console.log(array());
+
+
+// Vazifa-4
+
+
+// let company1 = (users) =>{
+//      users = [
+//         ['hikmatuloh',20],
+//         ['asiddin',18],
+//         ['firdavs',12]
+//        ]
+     
+//        for (let i =0; i<users.length;i++){
+//            let company =  prompt("kompaniya nomini irting")
+//            users[i][2]  = company
+//        }
+          
+//           console.log(users);
+// }
+// console.log(company1());
+                      
+//Vaifa-2
+
+// let arr1 =["salom"]
+// let arr2= [" nma gap"]
+// let newArr =[]
+
+// let array = (user) => {
+//     newArr = arr1+arr2
+//     return newArr.split()
+// }
+// console.log(array());
