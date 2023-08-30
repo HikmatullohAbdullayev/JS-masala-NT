1113909504319e66a33d273e60968d3ed77d594f
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-08-30 23:42:05 +0500

    Task done

---
+++ Task-1/js/main.js
@@ -0,0 +1,27 @@
+// import { taqqoslash } from "./support.js";
+// import { tenglik } from "./support.js";
+// import { sonlar } from "./support.js";
+// import { bolinuvchi } from "./support.js";
+// import { aylanma } from "./support.js";
+
+// 1-masala
+// let son = Number(prompt("son kirting"))
+// console.log(taqqoslash(son));
+
+// 2-masala
+// let son1 = Math.round(Math.random()*10)
+// let son2 = Math.round(Math.random()*10)
+// let son3 = Math.round(Math.random()*10)
+// console.log(tenglik(son1,son2,son3));
+
+// 3-masala
+// let son = Math.round(Math.random()*1000)
+// console.log(sonlar(son));
+
+// 4-masala 
+// let son = Math.round(Math.random()*100)
+// console.log(bolinuvchi(son));
+// console.log(son);
+
+// 5-masala 
+// console.log(aylanma(2,8));
---
+++ Task-1/js/support.js
@@ -0,0 +1,78 @@
+// 1-masala 
+// export function taqqoslash(son) {
+//     if (son > 10) {
+//         return son+2
+        
+//     } else if(son<10) {
+//         return son*2
+//     }else if (son == 10) {
+//         return 22
+//     }
+    
+// }
+// taqqoslash()
+
+
+// 2-masala 
+// export function tenglik(son1,son2,son3) {
+//     if (son1==son2) {
+//         return son3
+//     } else if (son1 == son3) {
+//         return son2 
+//     }
+//      else if (son2 == son3) {
+//         return son1
+        
+//     }else{
+//         return 0
+//     }
+     
+    
+// }
+// tenglik()
+
+
+// 3-masala 
+// export function sonlar(son){
+//     if (son<= 999) {
+//         if (son >= 10 && son < 100) {
+//             return 2
+//         } else if(son >= 100) {
+//             return 3
+//         }else if (son < 10){
+//             return 1
+//         }
+        
+//     } else  {
+//         return "3 xonalik son kirting"
+//     }
+// }
+// sonlar()
+
+// 4-masala 
+
+// function bolinuvchi ( son){
+//    let count = 0
+//     for (let i = 0; i <= son ; i++) {
+//         if (i % 5==0) {
+//             count += i
+//         }
+        
+//     }
+//     return count
+// }
+
+// 5-masala 
+// function aylanma(aSon,bSon) {
+//     let sum = 0
+//     for (let i = aSon ; i  < bSon; i++) {
+//        sum = sum + i
+        
+//     }
+//     return sum
+// }
+
+// export  {
+//     bolinuvchi,
+//      aylanma
+// }
